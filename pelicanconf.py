89067cce575d1f0cba86c6c9e4f85a4969e9b745
moulinux <stephane@moulinux.net>
2020-01-22 18:34:16 +0100

    GNU/Linux

--- pelicanconf.py
+++ pelicanconf.py
@@ -40,7 +40,7 @@ DEFAULT_PAGINATION = False
 THEME = 'themes/html5-dopetrope'
 STATIC_PATHS = ( "doc/", )
 
-LIBRE = 'Permettre à qui le souhaite, d\'explorer et d\'utiliser des systèmes d’exploitation informatiques non-propriétaires pour s’équiper en systèmes GNU, Linux, et en logiciels dits "libres".'
+LIBRE = 'Permettre à qui le souhaite, d\'explorer et d\'utiliser des systèmes d’exploitation informatiques non-propriétaires pour s’équiper en systèmes GNU/Linux, et en logiciels dits "libres".'
 SOLIDAIRE = 'Former et équiper des populations en difficulté ou défavorisées pour permettre l\'accès à l\'informatique et à Internet à tous.'
 ASSOCIATION = 'Pour bénéficier d\'un service, il est nécessaire d\'être membre de l\'association'
 ABOUT_TEXT = 'LIBRAISOL (Libre Association Informatique Solidaire Linux) est une association régie par la loi du 1er juillet 1901 et le décret du 16 août 1901.'
